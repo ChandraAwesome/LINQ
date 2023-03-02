9d70b1f5a5e47ec8c47046892c09889fe9830c90
ChandarAwesome <ajaygames00Gmail.com>
2023-03-02 08:42:20 +0530

    [Chandra]-LINQ

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Table demo");
+            LinqToDataTable linqToDataTable = new LinqToDataTable();
+            linqToDataTable.AddToDataTableDemo();
+            Console.ReadKey();
+        }
+    }
+}
